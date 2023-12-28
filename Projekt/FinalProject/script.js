be6a3d379f8bec0373f8f2a3a140cb948af30a01
Lara Pirdaoud <lara.pirdaoud@hotmail.com>
2023-12-28 19:50:00 +0100

    first commit

--- Projekt/FinalProject/script.js
+++ Projekt/FinalProject/script.js
@@ -21,25 +21,96 @@ window.onscroll = () =>{
 };
 
     // Contact Form
+
     document.addEventListener("DOMContentLoaded", function () {
-    let confirmationMessage = document.getElementById("confirmationMessage");
-    
-    
-        if (confirmationMessage) {
-            document.getElementById("contactForm").addEventListener("submit", function (event) {
-                event.preventDefault();
-    
-    
-                confirmationMessage.innerText = "Message sent!";
-    
-    
-                setTimeout(function () {
-                    confirmationMessage.innerText = "";
-                    document.getElementById("contactForm").reset();
-                }, 3000);
-            });
-        } else {
-            console.error("Element with ID 'confirmationMessage' not found!");
-        }
+        let confirmationMessage = document.getElementById("confirmationMessage");
+        
+        
+            if (confirmationMessage) {
+                document.getElementById("contactForm").addEventListener("submit", function (event) {
+                    event.preventDefault();
+        
+        
+                    confirmationMessage.innerText = "Message sent!";
+        
+        
+                    setTimeout(function () {
+                        confirmationMessage.innerText = "";
+                        document.getElementById("contactForm").reset();
+                    }, 3000);
+                });
+            } else {
+                console.error("Element with ID 'confirmationMessage' not found!");
+            }
+        });
+
+
+document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-    
+});
+
+// Navbar
+        
+document.addEventListener('DOMContentLoaded', function () {
+    function highlightActiveLink() {
+        const navbarLinks = document.querySelectorAll('.navbar a');
+
+        let lastSection = null;
+
+        navbarLinks.forEach((link) => {
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                const rect = targetSection.getBoundingClientRect();
+                const isInView = rect.top <= 50 && rect.bottom >= 50;
+
+                if (isInView) {
+                    lastSection = link;
+                }
+            }
+        });
+
+        const currentPage = window.location.pathname;
+
+        navbarLinks.forEach((link) => {
+            link.classList.remove('active');
+        });
+
+        if (currentPage.includes('contact.html')) {
+            navbarLinks.forEach((link) => {
+                const targetId = link.getAttribute('href').substring(1);
+                if (targetId === 'contact') {
+                    link.classList.add('active');
+                }
+            });
+        } else if (currentPage.includes('about.html')) {
+            navbarLinks.forEach((link) => {
+                const targetId = link.getAttribute('href').substring(1);
+                if (targetId === 'about') {
+                    link.classList.add('active');
+                }
+            });
+        } else if (lastSection) {
+            lastSection.classList.add('active');
+        }
+    }
+
+    window.addEventListener('scroll', highlightActiveLink);
+    highlightActiveLink();
+});
+
+
+
+
+
+
+
+              
+        
